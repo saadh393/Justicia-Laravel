e96d672f0a43c222df2c2e23d5ace042300cfb68
Saad from Linux <saadh393@gmail.com>
2022-06-11 12:36:54 +0600

    Updated Database

--- database/justiciaaDb.sql
+++ database/justiciaaDb.sql
@@ -43,31 +43,6 @@ LOCK TABLES `failed_jobs` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `join_us`
---
-
-DROP TABLE IF EXISTS `join_us`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `join_us` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(150) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `phone` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `join_us`
---
-
-LOCK TABLES `join_us` WRITE;
-/*!40000 ALTER TABLE `join_us` DISABLE KEYS */;
-/*!40000 ALTER TABLE `join_us` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `migrations`
 --
 
@@ -629,4 +604,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-11 12:32:23
+-- Dump completed on 2022-06-11 12:36:47
